1fefba625b923a5327a01ababf611e319a862858
Fatek1 <36360890+Fatek1@users.noreply.github.com>
2018-12-02 23:28:34 +0100

    oddcinek 6

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -7,10 +7,34 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
-            
-            //Console.WriteLine("Witaj");
-            Console.WriteLine("Witaj " + name);
+            Console.Write("Wpisz swoje imie ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj, " + name);
+
+            Console.Write("Podaj ile masz lat");
+
+            int age;
+            bool resault = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Brawo! Jesteś pełnoletni, możesz wypić browara");
+            }
+            else if (resault==false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wprowadziłeś nie poprawny wiek!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Możemy Ci zoproponować jedynie mleko");
+
+            }
+            Console.ReadKey();
         }
+
+        
     }
 }
