a09a584df4f42526fc325647cb5aa23d6c1388e3
Fatek1 <36360890+Fatek1@users.noreply.github.com>
2018-12-02 23:15:05 +0100

    Odcinek 6dfs

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -8,7 +8,7 @@ namespace Hello
         static void Main(string[] args)
         {
             string name = args[0];
-
+            
             //Console.WriteLine("Witaj");
             Console.WriteLine("Witaj " + name);
         }
