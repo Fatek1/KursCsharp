4c87336c5da962fe3a50439e2e60399329b2f023
Fatek1 <36360890+Fatek1@users.noreply.github.com>
2018-12-09 23:10:14 +0100

    Lekcja 6/35

--- PETLE/Program.cs
+++ PETLE/Program.cs
@@ -13,10 +13,10 @@ namespace PETLE
             //Pętle
             //https://www.youtube.com/watch?v=jAoizbTxWc4&list=PL6aekdNhY7DAeU-2LEGBDXls60Yy-Ubbu&index=5
 
-            int a = 0;
+            //int a = 0;
 
 
-            //while (a<10)
+            //while (a < 10)
             //{
             //    Console.WriteLine(a);
             //    a++;
@@ -28,12 +28,46 @@ namespace PETLE
             //    Console.WriteLine(DateTime.Now.TimeOfDay);
             //}
 
+            //do
+            //{
+            //    Console.WriteLine("Tekst");
+            //} while (false);
 
 
 
+
+
+            /*
+             * komentarze
+             * Ctrl+K Ctrl+D formatowanie kodu
+             * Ctrl+K Ctrl+C komentowanie zaznaczonego kodu
+             * Ctrl+K Ctrol+U usuwanie komnetarza z zaznaczonego kodu
+             */
+
+            //RZUTOWANIE ZMIENNYCH
+            //int i = int.MaxValue;
+            //double d = 5.1;
+
+            //i = (int)d;
+            //Console.WriteLine(i);
+
+            //WCZYTYWANIE DANYCH Z KONSOLI
+            //string s = Console.ReadLine();
+
+
+            //KONWERSJA TYPÓW (STRING>INT, INT>STRING)
+            //int i = int.Parse(s)+2;
+            //Console.WriteLine(i);
+
+            //s = i.ToString();
+
+            //Console.WriteLine(s);
+            //KNOSLA
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Witaj {0} \n","Sławek");
+            Console.Beep();
             Console.ReadKey();
-
-
         }
     }
 }
